aa647d02ff8779d7006dad4998ed448e7126025b
Maspur Mikrotik <maspurmikrotiker@gmail.com>
2018-11-20 06:48:37 +0700

    Update routeros.sh

--- routeros.sh
+++ routeros.sh
@@ -1,16 +1,10 @@
 apt-get update && \
-
 apt install -y gunzip && \
-
 apt install -y qemu-utils pv && \
-
-wget http://download2.mikrotik.com/routeros/6.42.9/chr-6.42.9.img.zip -O chr.img.zip
- && \
-
+wget http://download2.mikrotik.com/routeros/6.42.9/chr-6.42.9.img.zip -O chr.img.zip && \
+sleep 1 && \
 gunzip -c chr.img.zip > chr.img  && \
-
 qemu-img convert chr.img -O qcow2 chr.qcow2 && \
-
 qemu-img resize chr.qcow2 `fdisk /dev/vda -l | head -n 1 | cut -d',' -f 2 | cut -d' ' -f 2` && \
 modprobe nbd && \
 qemu-nbd -c /dev/nbd0 chr.qcow2 && \
